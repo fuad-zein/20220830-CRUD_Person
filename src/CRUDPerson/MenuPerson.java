46e43b25c029a839a287ac652aec89b6d08d8ea2
fuzi <fzein.1695@gmail.com>
2022-08-30 15:14:09 +0700

    crud person

---
+++ src/CRUDPerson/MenuPerson.java
@@ -0,0 +1,94 @@
+package CRUDPerson;
+
+import java.util.Scanner;
+
+public class MenuPerson {
+
+	//create menu
+	public static void menu(){
+		System.out.println("====================================");
+		System.out.println("Daftar Menu:");
+		System.out.println("====================================");
+		System.out.println("1: Add Person");
+		System.out.println("2: Delete Person");
+		System.out.println("3: Update Person");
+		System.out.println("4: Search Person");
+		System.out.println("5: Display Person");
+		System.out.println("6: Exit Menu");
+		System.out.println("====================================\n");
+		System.out.println("Masukkan menu yang diinginkan: ");
+	}
+
+	public static void main(String[] args){
+		PersonManagement hr = new PersonManagement();
+		Person person = new Person();
+
+		//data di awal
+		person.setId(1);
+		person.setName("Muhammad Nazli");
+		person.setAge(23);
+
+		//add data
+		hr.add(person);
+
+		Scanner input = new Scanner(System.in);
+
+		int option = 0;
+		do {
+			menu();
+			option = input.nextInt();
+
+			switch (option){
+				case 1:
+					System.out.print("Masukkan id = ");
+					int idPerson = input.nextInt();
+					System.out.print("Masukkan nama = ");
+					String namePerson = input.nextLine();
+					System.out.println();
+					System.out.print("Masukkan umur = ");
+					int agePerson = input.nextInt();
+
+					person = new Person(idPerson, namePerson, agePerson);
+					hr.add(person);
+					System.out.println(person.toString());
+					break;
+
+				case 2:
+					System.out.print("Masukkan id = ");
+					int rId = input.nextInt();
+
+					hr.delete(rId);
+					break;
+
+				case 3:
+					System.out.print("Masukkan id = ");
+					int rIdPerson = input.nextInt();
+					hr.update(rIdPerson, input);
+					break;
+
+				case 4:
+					System.out.print("Masukkan id = ");
+					int findId = input.nextInt();
+
+					if (!hr.find(findId)){
+						System.out.println("Data Person tidak ada!!!");
+					}
+					break;
+
+				case 5:
+					hr.display();
+					break;
+
+				case 6:
+					System.out.println("Terima kasih sudah berkunjung... :)");
+					System.exit(0);
+					break;
+
+				default:
+					System.out.println("Input salah...");
+					break;
+			}
+		}
+		while (option !=6);
+	}
+}
---
+++ src/CRUDPerson/PersonManagement.java
@@ -0,0 +1,102 @@
+package CRUDPerson;
+
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class PersonManagement {
+	LinkedList<Person> list;
+
+	public PersonManagement(){
+		list = new LinkedList<>();
+	}
+
+	// add data
+	public void add(Person person){
+		//checking data
+		if (!find(person.getId())){
+			list.add(person);
+		} else {
+			System.out.println("Data sudah ada di dalam list");
+		}
+	}
+
+	// search data
+	public boolean find(int id){
+		//iterasi person list
+		for (Person l : list){
+			//checking by id
+			if (l.getId() == id){
+				System.out.println(l);
+				return true;
+			}
+		}
+		return false;
+	}
+
+	//delete data
+	public  void delete(int recId){
+		Person personDel = null;
+
+		//iterasi person list
+		for (Person ll : list){
+			// finding data and delete by id
+			if (ll.getId() == recId){
+				personDel = ll;
+			}
+		}
+
+		// data null and show error message
+		if (personDel == null){
+			System.out.println("Data tidak ada!!!");
+		} else {
+			list.remove(personDel);
+			System.out.println("Data berhasil dihapus...");
+		}
+	}
+
+	//show person
+	public Person findPerson(int id){
+		//iterasi person list
+		for (Person l : list){
+			//checking data
+			if (l.getId() == id){
+				return l;
+			}
+		}
+		return null;
+	}
+
+	//update data
+	public void update(int id, Scanner input){
+		if (find(id)){
+			Person rec = findPerson(id);
+
+			System.out.print("Masukkan id terbaru = ");
+			int idPerson = input.nextInt();
+			System.out.print("Masukkan nama terbaru = ");
+			String namePerson = input.nextLine();
+			System.out.print("Masukkan umur terbaru = ");
+			int agePerson = input.nextInt();
+
+			rec.setId(id);
+			rec.setName(namePerson);
+			rec.setAge(agePerson);
+			System.out.println("Data berhasil diperbarui...");
+		} else {
+			System.out.println("Data tidak ada!!!");
+		}
+	}
+
+	// display data
+	public void display(){
+		// ketika data list kosong
+		if (list.isEmpty()){
+			System.out.println("Data tidak ada di list Person!!!\n");
+		}
+
+		// iterasi data list
+		for (Person person : list){
+			System.out.println(person.toString());
+		}
+	}
+}
